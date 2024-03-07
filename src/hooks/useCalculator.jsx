7b93a71ed00bb1bd4b31c54900156baefee42b7a
Alvin Ng <alvin.cl.ng@gmail.com>
2024-03-07 04:45:34 -0800

    UPdate code so it work with PEMAS

--- src/hooks/useCalculator.jsx
+++ src/hooks/useCalculator.jsx
@@ -4,10 +4,37 @@ const initialState = {
   screen: "0"
 }
 
+const sumArr = (arr) => {
+  const stack = [];
+  let num = 0;
+  let sign = '+';
+  for (let i = 0; i < arr.length; i++) {
+      const char = arr[i];
+      if (!isNaN(parseFloat(char))) {
+          num = num * 10 + parseFloat(char);
+      }
+      if ((isNaN(parseFloat(char)) && char !== ' ') || i === arr.length - 1) {
+          if (sign === '+') {
+              stack.push(num);
+          } else if (sign === '-') {
+              stack.push(-num);
+          } else if (sign === '*') {
+              stack.push(stack.pop() * num);
+          } else if (sign === '/') {
+              stack.push(Math.trunc(stack.pop() / num));
+          }
+          sign = char;
+          num = 0;
+      }
+  }
+  return stack.reduce((acc, val) => acc + val, 0);
+}
+
 function reducer(state, action) {
+  const allItems = state.screen.split(" ")
   switch(action.type) {
     case 'number-click':
-      const allItems = state.screen.split(" ")
+      
       if(allItems[allItems.length - 1].includes(".") && action.payload === ".") {
         return state
       }
@@ -37,8 +64,16 @@ function reducer(state, action) {
         }
       }
 
-      
+      if(action.payload === "=" && allItems.length === 1) {
+        return state
+      }
 
+      if(action.payload === "=") {
+        return {
+          ...state,
+          screen: `${sumArr(allItems)}`
+        }
+      }
 
       return state
     default: 
